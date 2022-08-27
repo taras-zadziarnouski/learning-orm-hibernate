db586b3f539421cd2f177a049e0669bf1ccd1078
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2022-08-27 11:44:11 +0400

    init commit

---
+++ src/main/java/by/zadziarnouski/learningormhibernate/LearningOrmHibernateApplication.java
@@ -0,0 +1,12 @@
+package by.zadziarnouski.learningormhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningOrmHibernateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearningOrmHibernateApplication.class, args);
+  }
+}
---
+++ src/test/java/by/zadziarnouski/learningormhibernate/LearningOrmHibernateApplicationTests.java
@@ -0,0 +1,12 @@
+package by.zadziarnouski.learningormhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningOrmHibernateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
